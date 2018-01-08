d777ecf18d21a864c7b752fe818c91c44528ce48
highslater <highslater@hotmail.com>
2018-01-08 02:13:18 -0500

    commit

---
+++ Python_Tutorial_FILES/Chapter_4/MORE_CONTROL_FLOW_TOOLS.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: UTF-8 -*-
+
+from platform import python_version
+from sys import hexversion
+
+print("The Python Version is:", python_version(), " #"+ str((hexversion)))
+print("\nMORE CONTROL FLOW TOOLS\n")
